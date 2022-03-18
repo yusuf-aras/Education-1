6544228a9956f05af448567a7fabc767e99b14a7
Yusuf ARAS <77542476+yusuf-aras@users.noreply.github.com>
2022-03-18 08:04:09 -0700

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// snippent
+namespace OPP1
+{   // CRUD Oparation
+    internal class Product // Entity : Varlık
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } // Urun Adı
+        public double UnitPrice { get; set; } // Satıs Fiyati
+        public int CategoryId { get; set; } // Kategory ID
+        public int UnitsInStock { get; set; } // Stok Adeti
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OPP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.ProductName = " Masa ";
+            product1.CategoryId = 1;
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+
+            Product product2 = new Product  // < - Dizi
+            {
+                ProductName = "Kalem", Id = 3 , UnitPrice = 300 , CategoryId = 4 , UnitsInStock = 51
+            };
+            // PascalCase -camelCase
+            // Case Sensitive
+            // stack      - heap
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            productManager.Add(product2);
+
+            int toplamaSonucu = productManager.Topla(3,9);
+
+            Console.WriteLine(toplamaSonucu*2);
+
+            // int , double , bool  > Deger Tip
+            // class , array , abstract class , interface > Referans tip
+            // ref - aut
+            // void  -> git yap bitir
+        }
+    }
+}
